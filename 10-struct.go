a0408cf8c33c1cf8cf2839f62414890344ade110
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-01-14 13:57:55 +0300

    added struct tutorial

---
+++ 10-struct.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+/*
+ * A struct is aggregate data type. it groups together other objects of abitrary type.
+ * 
+ */
+
+func main(){
+
+	// Strut Person which agregate types of name, address and phone as fields of type Person
+
+	type Person struct {
+		name string
+		address string
+		phone string
+	}
+
+	var tzPresident Person
+
+	// Accessing the struct fields, we use dot notation
+	tzPresident.name = "John Pombe Magufuli"
+	tzPresident.address = "Magogoni street"
+	tzPresident.phone ="(222) 0 1234445"
+
+	x := tzPresident.name
+
+	fmt.Println(x)
+
+	// Initializing struct we can use new() which will initialize all fields to zero data of that field type. if int, then 0 if string then ""
+	p2 := new(Person)
+
+	fmt.Println(p2)
+
+	// OR initialize using struct literal
+
+	p1 := Person{name: "joe", address: "Mazinge st.", phone: "0277337733"}
+
+	fmt.Println(p1)
+
+}
