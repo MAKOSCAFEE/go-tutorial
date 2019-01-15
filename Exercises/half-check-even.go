ce03557afe179712776fb28b40eb3716312968d0
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-01-15 23:34:34 +0300

    added largest number implementation

---
+++ Exercises/half-check-even.go
@@ -0,0 +1,6 @@
+/*
+ * @Author: Barnabas Makonda
+ * @Date: 2019-01-15 23:34:13
+ * @Last Modified by:   Barnabas Makonda
+ * @Last Modified time: 2019-01-15 23:34:13
+ */
--- Exercises/largest-number.go
+++ Exercises/largest-number.go
@@ -1,6 +1,30 @@
 /*
  * @Author: Barnabas Makonda
  * @Date: 2019-01-15 23:21:23
- * @Last Modified by:   Barnabas Makonda
- * @Last Modified time: 2019-01-15 23:21:23
+ * @Last Modified by: Barnabas Makonda
+ * @Last Modified time: 2019-01-15 23:32:46
  */
+package main
+
+import "fmt"
+
+/* Question:
+ * Write a function with one variadic parameter that finds the greatest number in a list of numbers.
+ *
+ */
+
+func largestNumber(args ...int) int {
+	largest := args[0]
+
+	for _, value := range args {
+		if value > largest {
+			largest = value
+		}
+	}
+
+	return largest
+}
+
+func main() {
+	fmt.Println(largestNumber(2, 68, 38, 90, 8))
+}
