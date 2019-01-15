8c70dc6b8acabdd83a238a63141f306dcad81efc
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-01-15 22:58:48 +0300

    added more stuffs in functions

--- 12-functions.go
+++ 12-functions.go
@@ -1,8 +1,8 @@
 /*
  * @Author: Barnabas Makonda
  * @Date: 2019-01-15 22:46:35
- * @Last Modified by:   Barnabas Makonda
- * @Last Modified time: 2019-01-15 22:46:35
+ * @Last Modified by: Barnabas Makonda
+ * @Last Modified time: 2019-01-15 22:55:54
  */
 
 package main
@@ -31,6 +31,20 @@ func avarageTotal(xs []float64) (float64, float64) {
 	return total, total / float64(len(xs))
 }
 
+// variadic functions
+// By using ... before the type name of the last parameter you can indicate that it takes zero or more of those parameters.
+// In this case we take zero or more ints.
+// We invoke the function like any other function except we can pass as many ints as we want.
+
+func add(args ...int) int {
+	total := 0
+	for _, v := range args {
+		total += v
+	}
+
+	return total
+}
+
 func main() {
 	xs := []float64{98, 93, 77, 82, 83}
 	fmt.Println(average(xs))
@@ -38,4 +52,15 @@ func main() {
 	total, avarage := avarageTotal(xs)
 
 	fmt.Println(total, avarage)
+
+	fmt.Println(add(1, 2, 3, 4))
+
+	// Closure - it is possible to create function within a function.
+	// and it can access other local variables
+	x := 0
+	increment := func() int {
+		x++
+		return x
+	}
+	fmt.Println(increment())
 }
