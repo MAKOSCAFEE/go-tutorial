c73df719c75f1db70979177e8945b7220cdb7d67
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-01-15 23:42:16 +0300

    added check number if even and half it

--- Exercises/half-check-even.go
+++ Exercises/half-check-even.go
@@ -1,6 +1,28 @@
 /*
  * @Author: Barnabas Makonda
  * @Date: 2019-01-15 23:34:13
- * @Last Modified by:   Barnabas Makonda
- * @Last Modified time: 2019-01-15 23:34:13
+ * @Last Modified by: Barnabas Makonda
+ * @Last Modified time: 2019-01-15 23:41:24
  */
+
+package main
+
+import "fmt"
+
+func isEven(number int) bool {
+	return number%2 == 0
+}
+
+func halfAndCheck(number int) (int, bool) {
+	halfANumber := number / 2
+
+	isEvenNumber := isEven(number)
+
+	return halfANumber, isEvenNumber
+}
+
+func main() {
+	half, isEven := halfAndCheck(1)
+
+	fmt.Println(half, isEven)
+}
